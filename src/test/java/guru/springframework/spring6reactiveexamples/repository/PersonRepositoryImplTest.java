13cd2161f7cde3e99826b97a2a93f5caf4dad8a1
Lydia Rennert <lydia.rennert@gmail.com>
2025-06-13 15:44:32 +0200

    lre: Add Flux examples

--- src/test/java/guru/springframework/spring6reactiveexamples/repository/PersonRepositoryImplTest.java
+++ src/test/java/guru/springframework/spring6reactiveexamples/repository/PersonRepositoryImplTest.java
@@ -2,7 +2,10 @@ package guru.springframework.spring6reactiveexamples.repository;
 
 import guru.springframework.spring6reactiveexamples.domain.Person;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 class PersonRepositoryImplTest {
 
@@ -30,5 +33,56 @@ class PersonRepositoryImplTest {
 
         personMono.map(Person::getFirstName)
                 .subscribe(System.out::println);
+    }
+
+    @Test
+    void testFluxBlockFirst() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Person person = personFlux.blockFirst();
+
+        System.out.println(person);
+    }
+
+    @Test
+    void testFluxSubscriber() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        personFlux.subscribe(System.out::println);
+    }
+
+    @Test
+    void testFluxMap() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        personFlux.map(Person::getFirstName).subscribe(System.out::println);
+    }
+
+    @Test
+    void testFluxToList() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        // convert a flux to a mono
+        Mono<List<Person>> listMono = personFlux.collectList();
+
+        listMono.subscribe(list ->
+                list.forEach(person ->
+                        System.out.println(person.getFirstName())
+                ));
+    }
+
+    @Test
+    void testFilterOnName() {
+        personRepository.findAll()
+                .filter(person -> person.getFirstName().equals("Fiona"))
+                .subscribe(person -> System.out.println(person.getFirstName()));
+    }
+
+    @Test
+    void testGetByName() {
+        Mono<Person> fionaMono = personRepository.findAll().filter(person -> person.getFirstName().equals("Fiona"))
+                .next();
+
+        fionaMono.subscribe(person -> System.out.println(person.getFirstName()));
     }
 }
