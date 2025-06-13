26c87ecad804ffaf04e4f29a5de4ab4d79598302
Lydia Rennert <lydia.rennert@gmail.com>
2025-06-13 22:04:46 +0200

    lre: Add in a StepVerifier to test Mono

--- src/test/java/guru/springframework/spring6reactiveexamples/repository/PersonRepositoryImplTest.java
+++ src/test/java/guru/springframework/spring6reactiveexamples/repository/PersonRepositoryImplTest.java
@@ -4,6 +4,7 @@ import guru.springframework.spring6reactiveexamples.domain.Person;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.util.List;
 
@@ -20,9 +21,27 @@ class PersonRepositoryImplTest {
     }
 
     @Test
+    void testGetByIdFoundStepVerifier() {
+        Mono<Person> personMono = personRepository.getById(4);
+
+        StepVerifier.create(personMono).expectNextCount(1).verifyComplete();
+
+        personMono.subscribe(person -> System.out.println(person.getFirstName()));
+    }
+
+    @Test
     void testGetByIdNotFound() {
         Mono<Person> personMono = personRepository.getById(6);
         assertThat(personMono.hasElement().block()).isFalse();
+    }
+
+    @Test
+    void testGetByIdNotFoundStepVerifier() {
+        Mono<Person> personMono = personRepository.getById(6);
+
+        StepVerifier.create(personMono).expectNextCount(0).verifyComplete();
+
+        personMono.subscribe(person -> System.out.println(person.getFirstName()));
     }
 
     @Test
